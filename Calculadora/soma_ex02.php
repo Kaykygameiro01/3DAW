3ace1bc213e637730ebc392901db37947f8753d6
Kayky Gameiro <kayky.22204708360060@faeterj-rio.edu.br>
2024-08-27 11:15:09 -0300

    Atualização Calculadora

--- Calculadora/soma_ex02.php
+++ Calculadora/soma_ex02.php
@@ -29,6 +29,56 @@ switch ($op) {
       break;
 
     } 
+    case 'inversao':
+      if($v1 != 0){
+        $result =  $v1 * -1;
+        break;
+       } else{
+         $result = $v2 * -1;
+         break;
+       } 
+         case '1/x':
+          if($v1 != NULL){
+            $result =  1/$v1;
+            break;
+           } else{
+           $result = 1/$v2;
+             break;
+           } 
+           case 'sen':
+            if($v1 != NULL){
+              $result =  sin($v1);
+              break;
+             } else{
+               $result =  sin($v2);
+               break;
+             } 
+             case 'cos':
+              if($v1 != NULL){
+                $result = cos($v1);
+                break;
+              } else{
+                $result = cos($v2);
+              }
+              case 'tan':
+                if($v1 != NULL){
+                  $result = tan($v1);
+                  break;
+                } else{
+                  $result = tan($v2);
+                }
+                case 'log':
+                  if($v1 != NULL){
+                    $result =  (log($v1)) ;
+                    break;
+                   } else{
+                     $result = (log($v2)) ;
+                     break;
+                   }
+
+                   default: 
+                   $result = 'Erro';
+                   break;
 }
 ?>
 
@@ -45,6 +95,8 @@ switch ($op) {
   <?php echo "<h1>Resultado: $result</h1>"; ?>
   <?php echo "<h1>Operador: $$op</h1>"; ?>
 
+
 </body>
+
 
 </html>
