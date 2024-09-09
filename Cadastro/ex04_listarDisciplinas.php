cb55bee5775958f814a4cc2b1ca4be100900f1cd
Kayky Gameiro <kayky.22204708360060@faeterj-rio.edu.br>
2024-09-08 22:48:45 -0300

    ex04

---
+++ Cadastro/ex04_listarDisciplinas.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+</head>
+
+<body>
+  <h1>Listar Disciplinas</h1>
+  <table>
+    <tr>
+      <th>Nome</th>
+      <th>Sigla</th>
+      <th>Carga</th>
+    </tr>
+    <?php
+    $arqDisc = fopen("disciplinas.txt", "r") or die("Erro ao abrir arquivo");
+
+    while (!feof($arqDisc)) {
+      $linha = fgets($arqDisc);
+      $colunaDados = explode(";", trim($linha));
+
+      if (count($colunaDados) == 3) {
+        echo "<tr><td>" . $colunaDados[0] . "</td>" .
+          "<td>" . $colunaDados[1] . "</td>" .
+          "<td>" . $colunaDados[2] . "</td></tr>";
+      }
+    }
+
+    fclose($arqDisc);
+    $msg = "Deu tudo certo!!!";
+    ?>
+  </table>
+  <p><?php echo $msg; ?></p>
+  <br>
+</body>
+
+</html>
