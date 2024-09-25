8477a3ffa77659fe70630d8549ef40cfb18975a1
Kayky Gameiro <kayky.22204708360060@faeterj-rio.edu.br>
2024-09-25 01:17:50 -0300

    a alteração aqui foi apenas botar o php dentro do html

--- prova/Listar_Perguntas.php
+++ prova/Listar_Perguntas.php
@@ -1,38 +1,19 @@
-
-    <?php
-    $arquivo = "perguntas.txt";
-    $linhas = file($arquivo, FILE_IGNORE_NEW_LINES);
-
-    foreach ($linhas as $linha) {
-      $dados = explode(';', $linha);
-      $id = $dados[0];
-      $pergunta = $dados[1];
-      $respostas = $dados[2];
-
-      echo "<td>$id</td>";
-      echo "<td>$pergunta</td>";
-      echo "<td>$respostas</td>";
-    }
-    ?>
-  </table>
-
-  <!DOCTYPE html>
+<!DOCTYPE html>
 <html>
 
 <head>
   <title>Listar Perguntas</title>
   <style>
-   
-    table, th, td {
+    table,
+    th,
+    td {
       border: 1px solid black;
       padding: 8px;
-
-    }
-    table{
-        width: 100%;
-
     }
 
+    table {
+      width: 100%;
+    }
   </style>
 </head>
 
@@ -44,6 +25,26 @@
       <th>Pergunta</th>
       <th>Respostas</th>
     </tr>
+
+    <?php
+    $arquivo = "perguntas.txt";
+    $linhas = file($arquivo, FILE_IGNORE_NEW_LINES);
+
+    foreach ($linhas as $linha) {
+      $dados = explode(';', $linha);
+      $id = $dados[0];
+      $pergunta = $dados[1];
+      $respostas = $dados[2];
+
+      echo "<tr>";
+      echo "<td>$id</td>";
+      echo "<td>$pergunta</td>";
+      echo "<td>$respostas</td>";
+      echo "</tr>";
+    }
+    ?>
+
+  </table>
 </body>
 
 </html>
